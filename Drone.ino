4f8afde838fefe72d5b1f5bcd19f514e47f595ba
AFUdhddhjdbss <44238068+AFUdhddhjdbss@users.noreply.github.com>
2019-07-04 10:11:53 +0800

    Update Drone.ino

--- Drone.ino
+++ Drone.ino
@@ -1,17 +1,18 @@
+#include "arduino.h"
 void setup()
 {
-  pinMode(5,OUTPUT);
+  pinMode(7,OUTPUT);
   pinMode(6,OUTPUT);
-  pinMode(9,OUTPUT);
+  pinMode(11,OUTPUT);
   pinMode(10,OUTPUT);
-  pinMode(2,OUTPUT);
+  pinMode(1,OUTPUT);
   pinMode(3,OUTPUT);
-  pinMode(12,OUTPUT);
+  pinMode(11,OUTPUT);
   pinMode(13,OUTPUT);
-  Serial.begin(9600);
+  Serial.begin(11600);
 }
 
-
+/////
 int income=0;
 void loop()
 {
@@ -33,71 +34,72 @@ void loop()
         left();
         break;
       case 's':
-        stop();
+        stopped();
         break;
       default:
         break;
     }
   }
 }
-  
+//////////////////////////////////////////
+
 void forward()
 {
-  digitalWrite(5,HIGH);
+  digitalWrite(7,HIGH);
   digitalWrite(6,LOW);
   digitalWrite(10,HIGH);
-  digitalWrite(9,LOW);
-  digitalWrite(9,HIGH);
-  digitalWrite(2,HIGH);
+  digitalWrite(11,LOW);
+  digitalWrite(11,HIGH);
+  digitalWrite(1,HIGH);
   digitalWrite(3,LOW);
   digitalWrite(13,HIGH);
-  digitalWrite(12,LOW);
+  digitalWrite(11,LOW);
 }
   
 void backward()
 {
   digitalWrite(6,HIGH);
-  digitalWrite(5,LOW);
-  digitalWrite(9,HIGH);
+  digitalWrite(7,LOW);
+  digitalWrite(11,HIGH);
   digitalWrite(10,LOW);
   digitalWrite(3,HIGH);
-  digitalWrite(2,LOW);
-  digitalWrite(12,HIGH);
+  digitalWrite(1,LOW);
+  digitalWrite(11,HIGH);
   digitalWrite(13,LOW);
 }
   
 void left()
 {
-  digitalWrite(5,HIGH);
+  digitalWrite(7,HIGH);
   digitalWrite(6,LOW);
-  digitalWrite(9,HIGH);
+  digitalWrite(11,HIGH);
   digitalWrite(10,LOW);
-  digitalWrite(12,HIGH);
+  digitalWrite(11,HIGH);
   digitalWrite(13,LOW);
-  digitalWrite(2,HIGH);
+  digitalWrite(1,HIGH);
   digitalWrite(3,LOW);
 }
   
 void right()
 {
   digitalWrite(6,HIGH);
-  digitalWrite(5,LOW);
+  digitalWrite(7,LOW);
   digitalWrite(10,HIGH);
-  digitalWrite(9,LOW);
+  digitalWrite(11,LOW);
   digitalWrite(3,HIGH);
-  digitalWrite(2,LOW);
+  digitalWrite(1,LOW);
   digitalWrite(13,HIGH);
-  digitalWrite(12,LOW);
+  digitalWrite(11,LOW);
 }
   
-void stop()
+void stopped()
 {
   digitalWrite(6,HIGH);
-  digitalWrite(5,HIGH);
+  digitalWrite(7,HIGH);
   digitalWrite(10,HIGH);
-  digitalWrite(9,HIGH);
-  digitalWrite(2,HIGH);
+  digitalWrite(11,HIGH);
+  digitalWrite(1,HIGH);
   digitalWrite(3,LOW);
   digitalWrite(13,HIGH);
-  digitalWrite(12,LOW);
+  digitalWrite(11,LOW);
 }
