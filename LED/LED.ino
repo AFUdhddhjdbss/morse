d4a356c113236e25d7790a7d19ab525bcfffbf66
AFUdhddhjdbss <44238068+AFUdhddhjdbss@users.noreply.github.com>
2019-07-05 12:19:23 +0800

    Update LED.ino

--- LED/LED.ino
+++ LED/LED.ino
@@ -1,5 +1,4 @@
-int income = 0;
-
+int income =30;
 void setup()
 {
   pinMode(2, OUTPUT);
@@ -8,12 +7,35 @@ void setup()
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
   Serial.begin(9600);
+  
 }
 
 void loop()
 {
+  if (Serial.available()>0)
+  {
+  income =Serial.read();
   digitalWrite(6, LOW);
-  digitalWrite(3, HIGH);
-  delay(3000);
+  income = income-'0';
+  setPin(5, income&1);
+  setPin(2, (income>>1)&1);
+  setPin(3, (income>>2)&1);
+  setPin(4, (income>>3)&1);
+
+  delay(30);
   digitalWrite(6, HIGH);
+  delay(10);
+  }
+}
+  
+void setPin(int pin,int v)
+{
+  if(v>0)
+  {
+    digitalWrite(pin, HIGH);
+  }
+  else
+  {
+    digitalWrite(pin, LOW);
+  }
 }
